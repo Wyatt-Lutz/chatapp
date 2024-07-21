df7ef74131358b4535abb70832a6e113b6cd4c05
kingmancho <wyattlutz4@gmail.com>
2024-07-21 01:10:23 -0400

    blocking users

--- src/routes/Main/Settings/DeleteAccount.jsx
+++ src/routes/Main/Settings/DeleteAccount.jsx
@@ -24,25 +24,25 @@ const DeleteAccount = ({changeDeletingAccountDisplayment}) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center p-6 bg-black bg-opacity-50">
-    <div className="relative w-full max-w-md p-6 bg-gray-600 rounded-lg shadow-lg">
-      <button onClick={() => changeDeletingAccountDisplayment(false)} className="absolute top-4 right-4"><Close /></button>
-      <h2 className="mb-4 text-lg font-semibold">Change your Password</h2>
+      <div className="relative w-full max-w-md p-6 bg-gray-600 rounded-lg shadow-lg">
+        <button onClick={() => changeDeletingAccountDisplayment(false)} className="absolute top-4 right-4"><Close /></button>
+        <h2 className="mb-4 text-lg font-semibold">Change your Password</h2>
 
-      <div>To delete your account, please enter your current password.</div>
+        <div>To delete your account, please enter your current password.</div>
 
-      <p>Password</p>
-      <input type="password" {...register('password')} />
+        <p>Password</p>
+        <input type="password" {...register('password')} />
 
-      <button className="bg-red-600">Delete</button>
+        <button className="bg-red-600">Delete</button>
 
 
-      <div className="flex justify-end space-x-2">
-        <button onClick={() => changeDeletingAccountDisplayment(false)} className="px-4 py-2 text-white">Cancel</button>
-        <button onClick={onDeleteAccountSubmit} className="px-4 py-2 text-white bg-indigo-500 rounded">Done</button>
+        <div className="flex justify-end space-x-2">
+          <button onClick={() => changeDeletingAccountDisplayment(false)} className="px-4 py-2 text-white">Cancel</button>
+          <button onClick={onDeleteAccountSubmit} className="px-4 py-2 text-white bg-indigo-500 rounded">Done</button>
+        </div>
       </div>
-    </div>
 
-  </div>
+    </div>
 
 
 
